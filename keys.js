586c91cd86960514f4a53a4658f6568255c896f5
erictriestocode <43325093+erictriestocode@users.noreply.github.com>
2019-02-13 21:41:10 -0600

    one more push before restarting my computer for updates

--- keys.js
+++ keys.js
@@ -4,3 +4,7 @@ exports.spotify = {
     id: process.env.SPOTIFY_ID,
     secret: process.env.SPOTIFY_SECRET
   };
+
+exports.bandsInTown = {
+  url: "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+}
