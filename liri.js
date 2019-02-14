9449060db0f9e9a9d3f8d7a9bcbd8fd773f59ef9
erictriestocode <eric.northrop@gmail.com>
2019-02-13 21:03:55 -0600

    main JS functions added with comments, package files set up

--- liri.js
+++ liri.js
@@ -1,3 +1,33 @@
 // Requirement as specified in the instructions
 require("dotenv").config();
 var keys = require("./keys.js");
+
+var input = process.argv[2];
+
+
+
+
+
+
+
+//  ***** FUNCTIONS *****
+
+// Function for searching for concerts 
+function concertSearch(){
+
+}
+
+// FUnction for searching for songs on Spotify
+function spotifySearch(){
+
+}
+
+// function for searching for Movies on OMDB
+function movieSearch(){
+
+}
+
+//  function for doing what it says
+function alfred(){
+
+}
