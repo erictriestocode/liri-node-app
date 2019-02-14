37944c64182ce481787dd1c71da11d6b3ee82d6b
Eric Northrop <eric.northrop@gmail.com>
2019-02-14 22:04:11 +0000

    attempting to start the concert search function

--- liri.js
+++ liri.js
@@ -13,6 +13,8 @@ console.log("DEBUG: You have this many inputs: " + inputLength);
 
 if (input[2] === "concert-this"){
     console.log("Concerts!")
+    var band = "blink-182";
+    concertSearch(band);
 }
 if (input[2] === "spotify-this-song"){
     console.log("Spotify!")
@@ -22,6 +24,8 @@ if (input[2] === "movie-this" ){
 }
 if (input[2] === "do-what-it-says"){
     console.log("DO MY WORK BITCH")
+} else{
+    console.log("Guess you dont want me to do SHIT! Go to Google Assistant or Alexa or Siri(lol)")
 }
 
 
@@ -46,14 +50,14 @@ function spotifySearch(){
 }
 
 // function for searching for Movies on OMDB
-// function movieSearch(){
+function movieSearch(){
 // if(){
 //     // get OMDB info using axios
 // } else {
 //     console.log("If you haven't watched 'Mr. Nobody,' then you should: <http://www.imdb.com/title/tt0485947/>");
 //     console.log("It's on Netflix!");
 // }
-// }
+}
 
 //  function for doing what it says
 function alfred(){
