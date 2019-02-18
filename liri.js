b865aa066914838dc829ca925a000d6cd0fc1228
erictriestocode <43325093+erictriestocode@users.noreply.github.com>
2019-02-18 14:30:51 -0600

    framing up the spotifySearch function

--- liri.js
+++ liri.js
@@ -6,6 +6,7 @@ require("dotenv").config();
 var keys = require("./keys.js");
 var axios = require("axios");
 var fs = require("fs");
+var spotify = require("node-spotify-api");
 
 var input = process.argv;
 var inputLength = process.argv.length - 2; //subtracting first two arguments(node path and file path)
@@ -17,7 +18,7 @@ var topLine = "*****************************************\n"
 var bottomLine = "\n*****************************************"
 
 console.log("DEBUG: ***** WELCOME TO LIRI *****");
-console.log("DEBUG: you have entered: " + input[2] + " and " + input[3]);
+console.log("DEBUG: you have entered: " + input[2] + " and " + userInput);
 console.log("DEBUG: You have this many inputs: " + inputLength);
 
 // If statements to parse input and direct to the correct functions
@@ -29,6 +30,7 @@ if (input[2] === "concert-this") {
 }
 if (input[2] === "spotify-this-song") {
     console.log("DEBUG: Spotify!")
+    spotifySearch(userInput);
 }
 if (input[2] === "movie-this") {
     console.log("DEBUG: Movies?")
@@ -47,40 +49,59 @@ function concertSearch(artist) {
     var concertDate;
 
 
-    console.log("DEBUG: you have entered: " + artist)
+    console.log("DEBUG: you have passed in: " + artist)
 
     axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
-        .then(function(response) {
+        .then(function (response) {
             venueName = response.data[0].venue.name;
             venueLocation = response.data[0].venue.city;
             concertDate = response.data[0].datetime;
 
-            // venueLocationc = response.data.
-                console.log (topLine);
-                console.log("CONCERT INFORMATION FOR " + artist);
-                console.log("NEXT EVENT: " + venueName);
-                console.log("EVENT LOCATION: " + venueLocation);
-                console.log("EVENT DATE: " + concertDate);
-                console.log(bottomLine);
+            // not ideal but for constructing this function it made more visual sense to me how this was done. Likely reduce it to one log statement.
+            console.log(topLine);
+            console.log("CONCERT INFORMATION FOR " + artist);
+            console.log("NEXT EVENT: " + venueName);
+            console.log("EVENT LOCATION: " + venueLocation);
+            console.log("EVENT DATE: " + concertDate);
+            console.log(bottomLine);
 
             // console Name of Venue
             // console venue location
             // console date of the event
-        }).catch(function(error) {
+        }).catch(function (error) {
             console.log(error);
         });
 
 }
 
 // Function for searching for songs on Spotify
-function spotifySearch() {
-    // I'll get here I promise
-}
+function spotifySearch(song) {
+    var userSong = song;
+
+    if (!userSong) {
+        userSong = "Ace of Bass" //I think this is spelled properly? Discussed in class that using the band name instead actually gave the desired results
+    }
+
+    // From Spotify Node API Documentation
+    // https://www.npmjs.com/package/node-spotify-api
+
+    spotify
+        .search({ type: 'track', query: userSong })
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
+};
+
+
+
 
 // function for searching for Movies on OMDB
-function movieSearch() {
+function movieSearch(movie) {
 
-
+    var movieInput = movie;
     // Taken largely from 10.2 levelTwoOmdbInteractive.js activity, modified for the homework
 
     // var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
@@ -95,8 +116,9 @@ function movieSearch() {
 
 }
 
-//  function for doing what it says
-function alfred() {
+// function for doing what it says
+// Named it Alfred after everyone's favorite butler
+function alfred(butler) {
     // need to install fs
     // spotifySearch();
 }
